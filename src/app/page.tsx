1de722561ecfe0f827f18ea06a2972676e64cb60
Bijay <bg00629@surrey.ac.uk>
2026-07-24 05:25:51 +0100

    pass 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,8 @@
-export default function Home() {
+import { db } from "../lib/db";
+
+export default async function Home() {
+  const result = await db.execute("SELECT * FROM entries");
+  console.log(result.rows);
   return (
     <h1>Journal</h1>
   );
