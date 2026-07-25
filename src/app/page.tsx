c75d0f82fef4727deaa2a197218e380d42accfe8
Bijay <bg00629@surrey.ac.uk>
2026-07-25 17:52:33 +0100

    Pass 3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
-import { db } from "../lib/db";
+import { getEntries } from "../lib/db";
 
 export default async function Home() {
-  const result = await db.execute("SELECT * FROM entries");
-  console.log(result.rows);
+  const entries = await getEntries();
+  console.log(entries);
+  const listItems = entries.map(entry => <li key={entry.id}>{entry.content} </li>);
   return (
+  <div>
     <h1>Journal</h1>
+    <ul>{listItems}</ul>
+  </div>
   );
 }
+
--- src/lib/db.ts
+++ src/lib/db.ts
@@ -3,3 +3,11 @@ import { createClient } from "@libsql/client";
 export const db = createClient({
 	url: "file:local.db"
 });
+
+interface Entry{id: number; content: string; created_at: string}
+
+export async function getEntries() {
+	const result = await db.execute("SELECT * FROM entries");
+	const entries = result.rows as unknown as Entry[];
+	return entries
+}
