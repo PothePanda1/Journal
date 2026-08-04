bea9a1da2dee3592cab5d0b6e7b663ee6a54b950
Bijay <bg00629@surrey.ac.uk>
2026-08-04 01:37:43 +0100

    produciton vs local

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,14 +1,13 @@
 import { createClient } from "@libsql/client";
 
-//checks if URL exists in env file
-if (!process.env.TURSO_DATABASE_URL) {
-    throw new Error("TURSO_DATABASE_URL is not set");
+if (process.env.NODE_ENV === "production" && !process.env.TURSO_DATABASE_URL) {
+	throw new Error("TURSO_DATABASE_URL must be set in production");
 }
 
-export const db = createClient({
-	url: process.env.TURSO_DATABASE_URL,
-	authToken: process.env.TURSO_AUTH_TOKEN
-});
+const url = process.env.TURSO_DATABASE_URL || "file:local.db";
+const authToken = process.env.TURSO_AUTH_TOKEN;
+
+export const db = createClient({ url, authToken });
 
 interface Entry{id: number; content: string; created_at: string}
 
